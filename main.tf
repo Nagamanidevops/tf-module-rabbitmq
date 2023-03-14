9fc0b3d0dadf40da063bd6680a9b4a7676acac70
nagamani <nagamanichavaaws@gmail.com>
2023-03-14 07:12:02 +0000

    f

--- main.tf
+++ main.tf
@@ -39,8 +39,7 @@ resource "aws_security_group" "rabbitmq" {
   
 resource "aws_mq_broker" "rabbitmq" {
   broker_name = "${var.env}-rabbitmq" 
-   deployment_mode    = var.deployment_mode
-
+   deployemnt_mode    = var.deployment_mode
   use_aws_owned_key = false
   kms_key_id = data.aws_kms_key.key.arm
     engine_type    = var.engine_type
