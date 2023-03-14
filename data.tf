53dc82b64ee64958eb6091f5cdb579feaa40559f
nagamani <nagamanichavaaws@gmail.com>
2023-03-14 09:04:55 +0000

    q

--- data.tf
+++ data.tf
@@ -4,9 +4,18 @@ data "aws_kms_key" "key" {
   key_id = "alias/roboshop"
 }
 
+# data "aws_ssm_parameter" "USER" {
+#   name = "${var.env}.rabbitmq.USER"
+  
+# }
+# data "aws_ssm_parameter" "PASS" {
+#   name = "${var.env}.rabbitmq.PASS"
+# }
+
 data "aws_ssm_parameter" "USER" {
-  name = "${var.env}.rabbitmq.user"
+  name = "${var.env}.rabbitmq.USER"
 }
+
 data "aws_ssm_parameter" "PASS" {
   name = "${var.env}.rabbitmq.PASS"
 }
