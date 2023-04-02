989d19e43611ee9b398a19e5ae8bbbd8aa77ce3a
nagamani <nagamanichavaaws@gmail.com>
2023-04-02 13:30:19 +0000

    as

--- main.tf
+++ main.tf
@@ -1,3 +1,103 @@
+resource "aws_iam_role" "role" {
+  name = "${var.env}-${var.component}-role"
+
+   assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+   tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-${var.component}-role" }
+  )
+}
+
+resource "aws_iam_instance_profile" "profile" {
+  name = "${var.env}-${var.component}-role"
+  role = aws_iam_role.role.name
+}
+
+
+resource "aws_security_group" "main" {
+  name        = "${var.env}-${var.component}-security-group"
+  description = "${var.env}-${var.component}-security-group"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description      = "HTTP"
+    from_port        = var.app_port
+    to_port          = var.app_port
+    protocol         = "tcp"
+    cidr_blocks      = var.allow_cidr
+  }
+  
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = var.bastion_cidr
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+   tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-${var.component}-security-group" }
+  )
+  
+  }
+  resource "aws_iam_policy" "policy" {
+  name        = "${var.env}-${var.component}-parameter-store-policy"
+  path        = "/"
+  description = "${var.env}-${var.component}-parameter-store-policy"
+
+  policy = jsonencode({
+  
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "ssm:GetParameterHistory",
+                "ssm:GetParametersByPath",
+                "ssm:GetParameters",
+                "ssm:GetParameter"
+            ],
+            "Resource": [
+            "arn:aws:ssm:us-east-1:998626143474:parameter/${var.env}.${var.component}*"
+              ]
+        },
+        {
+            "Sid": "VisualEditor1",
+            "Effect": "Allow",
+            "Action": "ssm:DescribeParameters",
+            "Resource": "*"
+        }
+    ]
+})
+   
+} 
+  
+  resource "aws_iam_role_policy_attachment" "role-attach" {
+  role       = aws_iam_role.role.name
+  policy_arn = aws_iam_policy.policy.arn
+}
 resource "aws_security_group" "rabbitmq" {
   name        = "${var.env}-rabbitmq-security-group"
   description = "${var.env}-rabbitmq-security-group"
@@ -113,7 +213,7 @@ resource "aws_spot_instance_request" "raabbitmq" {
   wait_for_fulfillment = true
     
   user_data = base64encode(templatefile("${path.module}/user-data.sh", { component = "rabbitmq", env = var.env }))
-
+  iam_instance_profile = aws_iam_instance_profile.name
   tags = merge(
     local.common_tags,
     { Name = "${var.env}-rabbitmq" }
